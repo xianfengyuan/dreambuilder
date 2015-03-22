fdddde5ce5c668cdfef18cd0424f691f8d8efeb0
Xianfeng Yuan <xyuan@samsung.com>
2015-03-21 20:20:27 -0700

    jira: dr-0000 master branch

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
 var fs = require('fs');
   
-function detectRunningProd() {
-    return fs.existsSync("/etc/motd") && fs.readFileSync("/etc/motd", "utf8").indexOf("OpsWorks Stack: moshpit-prod") > 1;
-}
-
-// we export the value, not the function: no need to evaluate it more than once
-exports.isRunningInProd = detectRunningProd(); 
-
 // jira credentials and project key
 exports.jira = {
     host: "jira.sheermount.com",
