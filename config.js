3e2e4823c17212738419d275aa3003e4a6aa6fcf
Xianfeng Yuan <xyuan@samsung.com>
2015-03-22 10:56:38 -0700

    fix view problem

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 var fs = require('fs');
   
-// jira credentials and project key
-exports.jira = {
-    host: "jira.sheermount.com",
-    port: "80",
-    username: "dreambuilder",
-    password: "dream123",
-    projectKey: exports.isRunningInProd ? "SIM": "DSIM"
+exports.dreambuilder = {
+    port: 4321
 };
