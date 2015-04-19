1a43e82803f6687fd9dcdcbb4a2af404a8abfb21
Xianfeng Yuan <xyuan@samsung.com>
2015-04-19 11:18:25 -0700

    update front page

--- react/sclient.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var Story = require('../components/Story.rc');
-
-// Render the components, picking up where react left off on the server
-React.render(
-  <Story/>,
-  document.getElementById('story')
-);
