6f34ecc09a2d923d0336b72a1fb829a4bb7a4538
Xianfeng Yuan <xyuan@samsung.com>
2015-06-05 17:52:15 -0700

    change port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 var app = express();
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8089);
 app.engine('html', hbs({
   defaultLayout: 'layout',
   layoutsDir: 'views',
